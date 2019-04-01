cc044ca9d84bff030b540b82e270b9a81e2f0828
th0mazzz <thomaszhao2001@gmail.com>
2019-04-01 15:42:13 -0400

    Got map api to show basic map

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask import request, session #login function
 from flask import url_for, redirect, flash #redirect functions
+from util import map
 
 import os, random
 
@@ -9,7 +10,8 @@ app.secret_key = os.urandom(32)
 
 @app.route("/")
 def root():
-    return render_template('home.html') 
+    
+    return render_template('home.html', link = map.displayMap()) 
 
 if __name__=="__main__":
     app.debug=True
---
+++ util/map.py
@@ -0,0 +1,6 @@
+import urllib
+from urllib import request
+
+def displayMap():
+    
+    return "https://www.mapquestapi.com/staticmap/v5/map?key=h0f9GfD3xpNsoelpFNawGNneZ25F61GC&center=Boston,MA&size=@2x"
