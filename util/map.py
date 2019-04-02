0017f8ac197b6f2d7a1563f6ef411b4d1aca10ca
Peter <pcwalina@stuy.edu>
2019-04-01 20:57:49 -0400

    got it to actually show nyc

--- util/map.py
+++ util/map.py
@@ -2,5 +2,5 @@ import urllib
 from urllib import request
 
 def displayMap():
-    
-    return "https://www.mapquestapi.com/staticmap/v5/map?key=h0f9GfD3xpNsoelpFNawGNneZ25F61GC&center=Boston,MA&size=@2x"
+
+    return "https://www.mapquestapi.com/staticmap/v5/map?key=h0f9GfD3xpNsoelpFNawGNneZ25F61GC&boundingBox=40.7628,-74.0560,40.6628,-73.956&size=500,500@2x"
