8b9ef52ba9864cea37235094abba692add5d9fdf
Peter <pcwalina@stuy.edu>
2019-04-02 13:43:30 -0400

    resolution

--- util/map.py
+++ util/map.py
@@ -3,4 +3,4 @@ from urllib import request
 
 def displayMap():
 
-    return "https://www.mapquestapi.com/staticmap/v5/map?key=h0f9GfD3xpNsoelpFNawGNneZ25F61GC&boundingBox=40.7628,-74.0560,40.6628,-73.956&size=500,500@2x"
+    return "https://www.mapquestapi.com/staticmap/v5/map?key=h0f9GfD3xpNsoelpFNawGNneZ25F61GC&boundingBox=40.7628,-74.0560,40.6628,-73.956&size=1920,1080@2x"
