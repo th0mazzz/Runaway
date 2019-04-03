9f38a4159865531880862d5fb5dfb714e3a509c1
Peter <pcwalina@stuy.edu>
2019-04-02 21:05:40 -0400

    something working, could be better

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template
 from flask import request, session #login function
 from flask import url_for, redirect, flash #redirect functions
-from util import map
+from util import map as m
 
-import os, random
+import os, random, json
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
+with open('data/stations.json', 'r') as file:
+    dict = json.load(file)
+    listOfStations = dict["stationBeanList"]
+
+
+
 @app.route("/")
 def root():
-    
-    return render_template('home.html', link = map.displayMap()) 
+    return render_template('home.html', link = m.displayMap(), list = listOfStations)
+
+@app.route("/<lat>/<long>")
+def map(lat,long):
+    return render_template('home.html', link = m.displayMapC(lat,long), list = listOfStations)
+
 
 if __name__=="__main__":
     app.debug=True
--- util/map.py
+++ util/map.py
@@ -2,5 +2,7 @@ import urllib
 from urllib import request
 
 def displayMap():
+    return "https://www.mapquestapi.com/staticmap/v5/map?key=h0f9GfD3xpNsoelpFNawGNneZ25F61GC&center=40.7643971,-73.97371465&zoom=10&size=@2x"
 
-    return "https://www.mapquestapi.com/staticmap/v5/map?key=h0f9GfD3xpNsoelpFNawGNneZ25F61GC&boundingBox=40.7628,-74.0560,40.6628,-73.956&size=1920,1080@2x"
+def displayMapC(lat,long):
+    return "https://www.mapquestapi.com/staticmap/v5/map?key=h0f9GfD3xpNsoelpFNawGNneZ25F61GC&center=" + lat + "," + long + "&zoom=15&size=@2x"
