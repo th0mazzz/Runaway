110fb609158e58be3d750b79ca8a67056df3afcd
Peter <pcwalina@stuy.edu>
2019-04-03 23:48:45 -0400

    something

--- static/charts.js
+++ static/charts.js
@@ -1,2 +1,16 @@
 var svg1 = document.getElementById("vimage");
-var stationId = svg1.getAttribute("stationid");
+var stationId = svg1.getAttribute("station");
+console.log(stationId)
+d3.json("https://raw.githubusercontent.com/th0mazzz/Runaway/master/data/stations.json").then(function(data) {
+  console.log("hi");
+  for (var x = 0;x < data.stationBeanList.length;x++){
+    if (stationId == data.stationBeanList[x].id){
+      console.log(data.stationBeanList[x]);
+      var docks = data.stationBeanList[x].totalDocks
+      var availableDocks = data.stationBeanList[x].availableDocks
+      var bikes = data.stationBeanList[x].availableBikes
+      var stationData = {docks:availableDocks,bikes:bikes,freeSpace:docks - (bikes + availableDocks)}
+      console.log(stationData);
+    }
+  }
+});
