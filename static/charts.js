6b82ccc4b8c44db11cef0d990a1d4be1caeb9a6e
thegamer5100 <pcwalina@stuy.edu>
2019-04-06 16:22:15 -0400

    managed to make a bar chart for the ages groups

--- static/charts.js
+++ static/charts.js
@@ -109,7 +109,158 @@ var dockStatusPieLegend = d3.select("#vimage")
         .style("font-size", 15)
 });
 
+//---------------------------------------- age bar -------------------------------------------
+var ageCsv = d3.csv("https://raw.githubusercontent.com/th0mazzz/Runaway/master/data/201503-citibike-tripdata.csv").then(function(data){
+  var i = 0;
+  var num10 = [];
+  var num20 = [];
+  var num30 = [];
+  var num40 = [];
+  var num50 = [];
+  var num60 = [];
+  var num70 = [];
+  var num80 = [];
+  var num90 = [];
+  var noAgegiven = [];
 
+
+  while(i < data.length){
+      //console.log(stationId);
+      //console.log("years");
+      //console.log(data[i]);
+      //so parsing the trips at this station
+      if(data[i]["start station id"] == stationId.toString() || data[i]["end station id"] == stationId.toString()){
+        //ages 0-10
+        if(2019 - parseInt(data[i]["birth year"],10) <= 10){
+          num10.push(data[i]);
+        }
+        //ages 11-20
+        else if(2019 - parseInt(data[i]["birth year"],10) <= 20){
+          num20.push(data[i]);
+        }
+        //ages 21-30
+        else if(2019 - parseInt(data[i]["birth year"],10) <= 30){
+          num30.push(data[i]);
+        }
+        //ages 31-40
+        else if(2019 - parseInt(data[i]["birth year"],10) <= 40){
+          num40.push(data[i]);
+        }
+        //ages 41-50
+        else if(2019 - parseInt(data[i]["birth year"],10) <= 50){
+          num50.push(data[i]);
+        }
+        //ages 51-60
+        else if(2019 - parseInt(data[i]["birth year"],10) <= 60){
+          num60.push(data[i]);
+        }
+        //ages 61-70
+        else if(2019 - parseInt(data[i]["birth year"],10) <= 70){
+          num70.push(data[i]);
+        }
+        //ages 71-80
+        else if(2019 - parseInt(data[i]["birth year"],10) <= 80){
+          num80.push(data[i]);
+        }
+        //ages 80+
+        else if(2019 - parseInt(data[i]["birth year"],10) <= 110){
+          num90.push(data[i]);
+        }
+        //if theres no age given throw em elsewhere
+        else{
+          noAgegiven.push(data[i]);
+        }
+      }
+      i++;
+  }
+  console.log(num10);
+  console.log(num20);
+  console.log(num30);
+  console.log(num40);
+  console.log(num50);
+  console.log(num60);
+  console.log(num70);
+  console.log(num80);
+  console.log(num90);
+  console.log(noAgegiven);
+
+  var ageData = [{"ageGroup": "0-10", "number": num10.length},
+                 {"ageGroup": "11-20", "number": num20.length},
+                 {"ageGroup": "21-30", "number": num30.length},
+                 {"ageGroup": "31-40", "number": num40.length},
+                 {"ageGroup": "41-50", "number": num50.length},
+                 {"ageGroup": "51-60", "number": num60.length},
+                 {"ageGroup": "61-70", "number": num70.length},
+                 {"ageGroup": "71-80", "number": num80.length},
+                 {"ageGroup": "80+", "number": num90.length},
+                 {"ageGroup": "N/A", "number": noAgegiven.length}];
+  console.log(ageData);
+
+  var svg = d3.select("#vimage"),
+      width = 400,
+      height = 300;
+
+  var x = d3.scaleBand().range([0,width]).padding(0.4).domain(ageData.map(function(d){return d.ageGroup;}));
+
+  var y = d3.scaleLinear().range([height,0]).domain([0,d3.max(ageData,function(d){return d.number;})]);
+
+  var g = svg.append("g")
+             .attr("transform", "translate("+ 100 + "," + 800 + ")");
+
+  g.append("g")
+   .attr("transform", "translate(0," + height + ")")
+   .call(d3.axisBottom(x))
+   .append("text")
+   .attr("y", height - 250)
+   .attr("x", width - 100)
+   .attr("text-anchor", "end")
+   .attr("stroke", "black")
+   .text("Age Group");
+
+  g.append("g")
+   .call(d3.axisLeft(y).tickFormat(function(d){
+     return d;
+   }).ticks(10))
+   .append("text")
+   .attr("transform", "rotate(-90)")
+   .attr("y", 6)
+   .attr("dy", "-5.1em")
+   .attr("text-anchor", "end")
+   .attr("stroke", "black")
+   .text("People");
+
+   var div = d3.select("body").append("div")
+       .attr("class", "tooltip")
+       .style("opacity", 0);
+
+
+  g.selectAll(".bar")
+    .data(ageData)
+    .enter().append("rect")
+    .attr("class","bar")
+    .attr("x", function(d){return x(d.ageGroup); })
+    .attr("y", function(d){return y(d.number); })
+    .attr("width", x.bandwidth())
+    .attr("height", function(d){return height - y(d.number); })
+    .on("mouseover", function(d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div	.html(d.number)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+            })
+    .on("mouseout", function(d) {
+          div.transition()
+              .duration(500)
+              .style("opacity", 0);
+        });
+
+
+
+
+
+});
 //---------------------------------------- Gender pie  ---------------------------------------
 
 
