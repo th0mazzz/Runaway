f0cf2ed208ea98a63344e982cb71dafabbf86859
th0mazzz <thomaszhao2001@gmail.com>
2019-04-08 14:04:18 -0400

    more efficient pie rendering

--- static/charts.js
+++ static/charts.js
@@ -7,14 +7,14 @@ var stationId = svg1.getAttribute("station");
 d3.json("https://raw.githubusercontent.com/th0mazzz/Runaway/master/data/stations.json").then(function(data) {
 
     for (var x = 0;x < data.stationBeanList.length;x++){
-	if (stationId == data.stationBeanList[x].id){
-	    console.log(data.stationBeanList[x]);
-	    var docks = data.stationBeanList[x].totalDocks;
-	    var availableDocks = data.stationBeanList[x].availableDocks;
-	    var bikes = data.stationBeanList[x].availableBikes;
-	    var stationData = [{"name": "Open Docks", "number": availableDocks}, {"name": "Occupied Docks", "number": docks - availableDocks - bikes}, {"name":"Available Bikes", "number":bikes}];
-	    //console.log(stationData);
-	}
+        if (stationId == data.stationBeanList[x].id){
+            console.log(data.stationBeanList[x]);
+            var docks = data.stationBeanList[x].totalDocks;
+            var availableDocks = data.stationBeanList[x].availableDocks;
+            var bikes = data.stationBeanList[x].availableBikes;
+            var stationData = [{"name": "Open Docks", "number": availableDocks}, {"name": "Occupied Docks", "number": docks - availableDocks - bikes}, {"name":"Available Bikes", "number":bikes}];
+            //console.log(stationData);
+        }
     }
 
     var dockStatusWidth = 380;
@@ -105,6 +105,249 @@ d3.json("https://raw.githubusercontent.com/th0mazzz/Runaway/master/data/stations
         .attr("class", "textselected")
         .style("text-anchor", "start")
         .style("font-size", 15)
+});
+
+//---------------------------------------- Gender pie  ---------------------------------------
+
+
+d3.csv("https://raw.githubusercontent.com/th0mazzz/Runaway/master/data/201503-citibike-tripdata.csv").then(function(data){
+    var i = 0;
+    var numMale = 0;
+    var numFem = 0;
+    var numNA = 0;
+    console.log(stationId)
+
+    while(i < data.length){
+        //console.log(stationId);
+        if(data[i]["start station id"] == stationId.toString() || data[i]["end station id"] == stationId.toString()){
+            if(data[i]["gender"] == "1"){
+                numMale++;
+            }
+            else{
+                if(data[i]["gender"] == "2"){
+                    numFem++;
+                }
+                else{
+                    numNA++;
+                }
+            }
+        }
+        i++;
+    }
+
+    console.log(numMale)
+    console.log(numFem);
+    console.log(numNA);
+
+    var genderData = [{"name": "Male", "number": numMale}, {"name": "Female", "number": numFem}, {"name":"N/A", "number":numNA}];
+
+    var genderWidth = 380;
+    var genderHeight = 380;
+    var genderRadius = 140
+    var genderDisplacementX = 850
+    var genderDisplacementY = 550
+
+    var genderColor = d3.scaleOrdinal()
+	.domain(genderData)
+	.range(["#52527a","#003399","#cc00cc"]);
+
+    var genderPie = d3.pie()
+	.value(function(d) {
+	    return d.number; })(genderData);
+
+    var genderArc = d3.arc()
+	.outerRadius(genderRadius - 10)
+	.innerRadius(0);
+
+    var genderLabelArc = d3.arc()
+	.outerRadius(genderRadius)
+	.innerRadius(genderRadius - 100);
+
+    var genderSvgPie = d3.select("#vimage")
+	.append("g")
+	.attr("transform", "translate(" + genderDisplacementX + "," + genderDisplacementY +")");
+
+    var genderArcy = genderSvgPie.selectAll("arc")
+	.data(genderPie)
+	.enter().append("g")
+	.attr("class", "arc");
+
+    var text = d3.select("#vimage")
+    .append("text")
+    .attr("x", genderDisplacementX - 90)
+    .attr("y", genderDisplacementY - 150)
+    .text("Gender Breakdown By Trip")
+    .style("font-size", 20)
+    .style("fill", "black")
+
+    genderArcy.append("path")
+	.attr("d", genderArc)
+	.style("fill", function(d) {
+	    //console.log(d);
+	    return genderColor(d.data.name);
+	});
+
+    genderArcy.append("text")
+	.attr("transform", function(d) { return "translate(" + genderLabelArc.centroid(d) + ")"; })
+	.text(function(d) { return d.data.number;})
+	.style("fill", "black");
+
+
+//---------------------------------------- Legend for Gender Pie Chart ---------------------------------------
+
+var genderPieLegend = d3.select("#vimage")
+.append("g")
+.attr("transform", "translate(" + genderDisplacementX + "," + genderDisplacementY +")");
+
+
+var legendElements = genderPieLegend.selectAll("#vimage")
+    .data(genderData)
+    .enter().append('g')
+    .attr("class", "genderLegend")
+    .attr("transform", function (d, i) {
+        {
+            return "translate(" + 200 +"," + (i * 20 - 60) + ")"
+        }
+    });
+
+    legendElements.append('rect')
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", function (d, i) {
+	    //console.log(i)
+	    return genderColor(i)
+        })
+
+    legendElements.append('text')
+        .attr("x", 20)
+        .attr("y", 10)
+    //.attr("dy", ".35em")
+        .text(function (d, i) {
+            return d.name
+        })
+        .attr("class", "textselected")
+        .style("text-anchor", "start")
+        .style("font-size", 15);
+
+
+//---------------------------------------- Customer/Subscriber pie  ---------------------------------------
+
+    var i = 0;
+    var numCust = 0;
+    var numSub = 0;
+    console.log(stationId)
+
+    while(i < data.length){
+        //console.log(stationId);
+        if(data[i]["start station id"] == stationId.toString() || data[i]["end station id"] == stationId.toString()){
+            if(data[i]["usertype"] == "Customer"){
+                numCust++;
+            }
+            else{
+                numSub++;
+            }
+        }
+        i++;
+    }
+
+    console.log(numCust)
+    console.log(numSub);
+
+    var userTypeData = [{"name": "customer", "number": numCust}, {"name": "subscriber", "number": numSub}];
+
+    var userTypeWidth = 380;
+    var userTypeHeight = 380;
+    var userTypeRadius = 140
+    var userTypeDisplacementX = 850
+    var userTypeDisplacementY = 900
+
+    var userTypeColor = d3.scaleOrdinal()
+	.domain(userTypeData)
+	.range(["#ff9966","#9966ff"]);
+
+    var userTypePie = d3.pie()
+	.value(function(d) {
+	    return d.number; })(userTypeData);
+
+    var userTypeArc = d3.arc()
+	.outerRadius(userTypeRadius - 10)
+	.innerRadius(0);
+
+    var userTypeLabelArc = d3.arc()
+	.outerRadius(userTypeRadius)
+	.innerRadius(userTypeRadius - 100);
+
+    var userTypeSvgPie = d3.select("#vimage")
+	.append("g")
+	.attr("transform", "translate(" + userTypeDisplacementX + "," + userTypeDisplacementY +")");
+
+    var userTypeArcy = userTypeSvgPie.selectAll("arc")
+	.data(userTypePie)
+	.enter().append("g")
+	.attr("class", "arc");
+
+    var text = d3.select("#vimage")
+    .append("text")
+    .attr("x", userTypeDisplacementX - 120)
+    .attr("y", userTypeDisplacementY - 150)
+    .text("Customers vs. Subscribers at this Station")
+    .style("font-size", 20)
+    .style("fill", "black")
+
+    //<text x="800" y="50" font-family="sans-serif" font-size="20px" fill="white">Dock Status!</text>
+
+    userTypeArcy.append("path")
+	.attr("d", userTypeArc)
+	.style("fill", function(d) {
+	    //console.log(d);
+	    return userTypeColor(d.data.name);
+	});
+
+    userTypeArcy.append("text")
+	.attr("transform", function(d) { return "translate(" + userTypeLabelArc.centroid(d) + ")"; })
+	.text(function(d) { return d.data.number;})
+	.style("fill", "black");
+
+
+//---------------------------------------- Legend for user Type Pie Chart ---------------------------------------
+
+var userTypePieLegend = d3.select("#vimage")
+.append("g")
+.attr("transform", "translate(" + userTypeDisplacementX + "," + userTypeDisplacementY +")");
+
+
+var legendElements = userTypePieLegend.selectAll("#vimage")
+    .data(userTypeData)
+    .enter().append('g')
+    .attr("class", "userTypeLegend")
+    .attr("transform", function (d, i) {
+        {
+            return "translate(" + 200 +"," + (i * 20 - 60) + ")"
+        }
+    });
+
+    legendElements.append('rect')
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", function (d, i) {
+	    //console.log(i)
+	    return userTypeColor(i)
+        })
+
+    legendElements.append('text')
+        .attr("x", 20)
+        .attr("y", 10)
+    //.attr("dy", ".35em")
+        .text(function (d, i) {
+            return d.name
+        })
+        .attr("class", "textselected")
+        .style("text-anchor", "start")
+        .style("font-size", 15);
 });
 
 //---------------------------------------- age bar -------------------------------------------
@@ -500,249 +743,4 @@ var ageCsv = d3.csv("https://raw.githubusercontent.com/th0mazzz/Runaway/master/d
 
 
 
-});
-//---------------------------------------- Gender pie  ---------------------------------------
-
-
-d3.csv("https://raw.githubusercontent.com/th0mazzz/Runaway/master/data/201503-citibike-tripdata.csv").then(function(data){
-    var i = 0;
-    var numMale = 0;
-    var numFem = 0;
-    var numNA = 0;
-    console.log(stationId)
-
-    while(i < data.length){
-        //console.log(stationId);
-        if(data[i]["start station id"] == stationId.toString() || data[i]["end station id"] == stationId.toString()){
-            if(data[i]["gender"] == "1"){
-                numMale++;
-            }
-            else{
-                if(data[i]["gender"] == "2"){
-                    numFem++;
-                }
-                else{
-                    numNA++;
-                }
-            }
-        }
-        i++;
-    }
-
-    console.log(numMale)
-    console.log(numFem);
-    console.log(numNA);
-
-    var genderData = [{"name": "Male", "number": numMale}, {"name": "Female", "number": numFem}, {"name":"N/A", "number":numNA}];
-
-    var genderWidth = 380;
-    var genderHeight = 380;
-    var genderRadius = 140
-    var genderDisplacementX = 850
-    var genderDisplacementY = 550
-
-    var genderColor = d3.scaleOrdinal()
-	.domain(genderData)
-	.range(["#52527a","#003399","#cc00cc"]);
-
-    var genderPie = d3.pie()
-	.value(function(d) {
-	    return d.number; })(genderData);
-
-    var genderArc = d3.arc()
-	.outerRadius(genderRadius - 10)
-	.innerRadius(0);
-
-    var genderLabelArc = d3.arc()
-	.outerRadius(genderRadius)
-	.innerRadius(genderRadius - 100);
-
-    var genderSvgPie = d3.select("#vimage")
-	.append("g")
-	.attr("transform", "translate(" + genderDisplacementX + "," + genderDisplacementY +")");
-
-    var genderArcy = genderSvgPie.selectAll("arc")
-	.data(genderPie)
-	.enter().append("g")
-	.attr("class", "arc");
-
-    var text = d3.select("#vimage")
-    .append("text")
-    .attr("x", genderDisplacementX - 90)
-    .attr("y", genderDisplacementY - 150)
-    .text("Gender Breakdown By Trip")
-    .style("font-size", 20)
-    .style("fill", "black")
-
-    genderArcy.append("path")
-	.attr("d", genderArc)
-	.style("fill", function(d) {
-	    //console.log(d);
-	    return genderColor(d.data.name);
-	});
-
-    genderArcy.append("text")
-	.attr("transform", function(d) { return "translate(" + genderLabelArc.centroid(d) + ")"; })
-	.text(function(d) { return d.data.number;})
-	.style("fill", "black");
-
-
-//---------------------------------------- Legend for Gender Pie Chart ---------------------------------------
-
-var genderPieLegend = d3.select("#vimage")
-.append("g")
-.attr("transform", "translate(" + genderDisplacementX + "," + genderDisplacementY +")");
-
-
-var legendElements = genderPieLegend.selectAll("#vimage")
-    .data(genderData)
-    .enter().append('g')
-    .attr("class", "genderLegend")
-    .attr("transform", function (d, i) {
-        {
-            return "translate(" + 200 +"," + (i * 20 - 60) + ")"
-        }
-    });
-
-    legendElements.append('rect')
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", 10)
-        .attr("height", 10)
-        .style("fill", function (d, i) {
-	    //console.log(i)
-	    return genderColor(i)
-        })
-
-    legendElements.append('text')
-        .attr("x", 20)
-        .attr("y", 10)
-    //.attr("dy", ".35em")
-        .text(function (d, i) {
-            return d.name
-        })
-        .attr("class", "textselected")
-        .style("text-anchor", "start")
-        .style("font-size", 15);
-});
-
-
-//---------------------------------------- Customer/Subscriber pie  ---------------------------------------
-
-
-d3.csv("https://raw.githubusercontent.com/th0mazzz/Runaway/master/data/201503-citibike-tripdata.csv").then(function(data){
-    var i = 0;
-    var numCust = 0;
-    var numSub = 0;
-    console.log(stationId)
-
-    while(i < data.length){
-        //console.log(stationId);
-        if(data[i]["start station id"] == stationId.toString() || data[i]["end station id"] == stationId.toString()){
-            if(data[i]["usertype"] == "Customer"){
-                numCust++;
-            }
-            else{
-                numSub++;
-            }
-        }
-        i++;
-    }
-
-    console.log(numCust)
-    console.log(numSub);
-
-    var userTypeData = [{"name": "customer", "number": numCust}, {"name": "subscriber", "number": numSub}];
-
-    var userTypeWidth = 380;
-    var userTypeHeight = 380;
-    var userTypeRadius = 140
-    var userTypeDisplacementX = 850
-    var userTypeDisplacementY = 900
-
-    var userTypeColor = d3.scaleOrdinal()
-	.domain(userTypeData)
-	.range(["#ff9966","#9966ff"]);
-
-    var userTypePie = d3.pie()
-	.value(function(d) {
-	    return d.number; })(userTypeData);
-
-    var userTypeArc = d3.arc()
-	.outerRadius(userTypeRadius - 10)
-	.innerRadius(0);
-
-    var userTypeLabelArc = d3.arc()
-	.outerRadius(userTypeRadius)
-	.innerRadius(userTypeRadius - 100);
-
-    var userTypeSvgPie = d3.select("#vimage")
-	.append("g")
-	.attr("transform", "translate(" + userTypeDisplacementX + "," + userTypeDisplacementY +")");
-
-    var userTypeArcy = userTypeSvgPie.selectAll("arc")
-	.data(userTypePie)
-	.enter().append("g")
-	.attr("class", "arc");
-
-    var text = d3.select("#vimage")
-    .append("text")
-    .attr("x", userTypeDisplacementX - 120)
-    .attr("y", userTypeDisplacementY - 150)
-    .text("Customers vs. Subscribers at this Station")
-    .style("font-size", 20)
-    .style("fill", "black")
-
-    //<text x="800" y="50" font-family="sans-serif" font-size="20px" fill="white">Dock Status!</text>
-
-    userTypeArcy.append("path")
-	.attr("d", userTypeArc)
-	.style("fill", function(d) {
-	    //console.log(d);
-	    return userTypeColor(d.data.name);
-	});
-
-    userTypeArcy.append("text")
-	.attr("transform", function(d) { return "translate(" + userTypeLabelArc.centroid(d) + ")"; })
-	.text(function(d) { return d.data.number;})
-	.style("fill", "black");
-
-
-//---------------------------------------- Legend for user Type Pie Chart ---------------------------------------
-
-var userTypePieLegend = d3.select("#vimage")
-.append("g")
-.attr("transform", "translate(" + userTypeDisplacementX + "," + userTypeDisplacementY +")");
-
-
-var legendElements = userTypePieLegend.selectAll("#vimage")
-    .data(userTypeData)
-    .enter().append('g')
-    .attr("class", "userTypeLegend")
-    .attr("transform", function (d, i) {
-        {
-            return "translate(" + 200 +"," + (i * 20 - 60) + ")"
-        }
-    });
-
-    legendElements.append('rect')
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", 10)
-        .attr("height", 10)
-        .style("fill", function (d, i) {
-	    //console.log(i)
-	    return userTypeColor(i)
-        })
-
-    legendElements.append('text')
-        .attr("x", 20)
-        .attr("y", 10)
-    //.attr("dy", ".35em")
-        .text(function (d, i) {
-            return d.name
-        })
-        .attr("class", "textselected")
-        .style("text-anchor", "start")
-        .style("font-size", 15);
 });
